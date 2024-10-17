79fd660c08d794e3b8e7b9a6e25e9808c60a3378
Rahul Dubey <111676105+Rahuldubey26@users.noreply.github.com>
2024-10-17 17:18:50 +0000

    commit

--- Day-one-reactjs/src/comppnents/PostList.jsx
+++ Day-one-reactjs/src/comppnents/PostList.jsx
@@ -2,19 +2,30 @@ import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 import Modal from "./Modal";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function PostList({ isPosting, onStopPosting }) {
-  const [posts, setPost] = useState([]);
+  const [posts, setPosts] = useState([]);
+
+  // it take a function and an array
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+    fetchPosts();
+  }, []);
+
   function addPostHandler(postData) {
     fetch("http://localhost:8080/posts", {
       method: "POST",
       body: JSON.stringify(postData),
-      header: {
+      headers: {
         "Content-Type": "application/json",
       },
     });
-    setPost((existingPost) => [postData, ...existingPost]);
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
 
   return (
